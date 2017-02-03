acf8f0cf7f747c707253950fc6613cc654de1db1
Matias Juguera <mati.juguera@gmail.com>
2017-02-03 19:10:23 -0300

    Cambios en la clase ProbandoConstante y se agrega la clase Cast

---
+++ Primeras Practicas/Cast.java
@@ -0,0 +1,10 @@
+
+class Cast {
+	public static void main(String[] args){
+		double d = 10;
+		int i = (int)d; //convierte el tipo de dato double a int. "(tipo)nombrevariable".
+		char hola = 'c';
+		int b = (int)hola; //convierte el char a int en este caso c=99.
+		System.out.println(b);
+	}
+}
