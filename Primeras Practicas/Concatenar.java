966a689c6d0af491a5d3c03eddd2e13a3c98d0a3
Matias Juguera <mati.juguera@gmail.com>
2017-02-03 19:35:58 -0300

    Agregado forEach y EjemploPrePost

---
+++ Primeras Practicas/Concatenar.java
@@ -0,0 +1,7 @@
+
+class Concatenar {
+	public static void main(String[] args){
+		String nombre = "jorge";
+		System.out.println("Hola" + nombre ); // suma de variables
+	}
+}
---
+++ Primeras Practicas/ForEach.java
@@ -0,0 +1,9 @@
+
+class ForEach {
+	public static void main(String[] args){
+		int[] conjuntoEnteros = {1,5,7};
+		for(int i : conjuntoEnteros){
+			System.out.println(i);
+		}
+	}
+}
